fddbc17c9259c303786a6ec52075ab223471eebd
Melany Dalama <melinahir.md@gmail.com>
2023-09-25 13:28:51 -0300

    link pacto dei

--- js/Home.js
+++ js/Home.js
@@ -1,6 +1,8 @@
-const body = document.querySelector(".body");
 const menuOpener = document.querySelector(".imagen-menu");
 const menu = document.querySelector(".lateral-menu");
+
+
+const contenedorImg = document.querySelector(".container-img");
 
 // Detecta cuando abrir y cuando cerrar el menu
 menuOpener.addEventListener('click', (e) => {
@@ -10,55 +12,14 @@ menuOpener.addEventListener('click', (e) => {
         menu.style.left = '0px'; // lo muestra
 });
 
+
 document.addEventListener('click', (e) => {
     if (e.target != menuOpener)
         menu.style.left = '-400px'; // lo esconde
 });
 
-$(document).ready(function() {
 
- const button = $(".button-firma-ya");
- const textoPacto = $(".texto-proyecto-2");
- const logo = document.querySelector(".logo");
- const menuOpener = document.querySelector(".imagen-menu");
-
- button.click(function(){
-
-    window.open("https://drive.google.com/file/d/14BJRo-4ccJtPsZRs3VefswFfxTZDFfxV/view?usp=sharing", "_blank");
-
-
-        });
-
-      /*  textoPacto.hover(
-            function() {
-                $(this).find("p").css("font-size", "1.7rem");
-                $(this).find("p").css("transition", "all 1s");
-                $(this).css("opacity", "0.9");
-
-            },
-            function() {
-                
-                $(this).find("p").css("font-size", "1.6rem"); 
-                $(this).css("opacity", "1");
-            }
-        );
-*/
-/*window.addEventListener('scroll', function() {
-
-    const encabezado = document.querySelector('.encabezado');
-
-    if (window.scrollY > 100) {
-        logo.style.transition = "transform 0.5s ease";
-        logo.style.height = '40px';
-        encabezado.style.height = '70px';
-        menuOpener.style.height = '28px';
-        menu.style.marginTop = '70px';
-
-    } else {
-        logo.style.height = '60px';
-        encabezado.style.height = '80px';
-        menu.style.marginTop = '80px';
-    }
-});*/
-
+contenedorImg.addEventListener('click', (e) => {
+            window.open('https://drive.google.com/file/d/14BJRo-4ccJtPsZRs3VefswFfxTZDFfxV/view?usp=sharing', '_blank');
 });
+           
